50fde6f3d8a8f2b37d9f756b448db4b9c9037ecf
transccc <135663889+transccc@users.noreply.github.com>
2024-07-02 11:47:52 +0200

    Update Format_for_InfluxDB.js

--- Node-RED-Functions/Format_for_InfluxDB.js
+++ Node-RED-Functions/Format_for_InfluxDB.js
@@ -1,17 +1,18 @@
-var temperature = parseFloat(msg.payload.temperature);
+// Convert payload data into floats
+var temperature = parseFloat(msg.payload.temperature); 
 var humidity = parseFloat(msg.payload.humidity);
 var reed_status = parseFloat(msg.payload.reed_status); 
-
+// Check if data valid
 if (isNaN(temperature) || isNaN(humidity)) {
     node.error("Payload is missing required numerical fields", msg);
     return null;
 }
-
+// Create a new payload object with converted float values
 msg.payload = {
     temperature: temperature,
     humidity: humidity,
     reed_status: reed_status  
 };
-
+//Set the measurement name for InfluxDB
 msg.measurement = "sensor_data";
 return msg;
